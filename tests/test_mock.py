ee4555b3d5eceeb97d5e629ad19ab57d806e7cf3
Massipssa <kerrache.massipssa@gmail.com>
2020-07-25 15:32:08 +0200

    Update test_mock.py

--- tests/test_mock.py
+++ tests/test_mock.py
@@ -6,7 +6,7 @@ from src.test_learn import rm
 class RmTestMock(unittest.TestCase):
 
     filename = "../conf/test.txt"
-
+    """
     def setUp(self):
         with open(self.filename, 'w') as file:
             file.write('Delete me')
@@ -16,7 +16,6 @@ class RmTestMock(unittest.TestCase):
         rm("any path")
         # test that rm called os.remove with the right parameters
         mock_os.remove.assert_called_with("any path")
-        """
         rm(self.filename)
         self.assertFalse(os.path.isfile(self.filename), "Failed to remove the file")
-        """
+    """
